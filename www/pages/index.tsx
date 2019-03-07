754845dd76fbac4fff977dc8b821624d64ed07cf
Andy Kay <andykay.me@gmail.com>
2019-03-07 13:47:08 -0500

    typescript and 💅 styled-components setup

---
+++ www/pages/index.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: #f800ff;
+  font-family: "Helvetica Neue";
+  font-size: 50px;
+`;
+
+export default () => <Title>✨Highlight</Title>;
