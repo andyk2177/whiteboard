00114f04f9b911fddc89d6d4a2c5512dd08a865c
Andy Kay <andykay.me@gmail.com>
2019-03-07 14:09:24 -0500

    logo

--- www/pages/index.tsx
+++ www/pages/index.tsx
@@ -3,7 +3,11 @@ import styled from "styled-components";
 const Title = styled.h1`
   color: #f800ff;
   font-family: "Helvetica Neue";
-  font-size: 50px;
+  padding: 0 20px;
 `;
 
-export default () => <Title>✨Highlight</Title>;
+export default () => (
+  <Title>
+    ✨ <img height="25" src="/static/highlight@2x.png" alt="HIGHLIGHT" />
+  </Title>
+);
